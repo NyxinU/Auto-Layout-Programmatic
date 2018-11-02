0258139f7c7bc8a8e01a114d54192232a3b05fda
Nix <yiunixon@gmail.com>
2018-11-02 15:51:12 -0700

    add image, center x & y, set width & height to 100

--- Auto Layout Programmatic/ViewController.swift
+++ Auto Layout Programmatic/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .black
+        let image = UIImage(named: "candyBar")
+        let imageView = UIImageView(image: image)
+        view.addSubview(imageView)
+        
+        // enables autolayout for imageView
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
 

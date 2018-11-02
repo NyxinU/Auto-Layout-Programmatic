bf611d5cea89e779a8dadf43572ce8f08f0f9a39
Nix <yiunixon@gmail.com>
2018-11-02 15:08:24 -0700

    start

---
+++ Auto Layout Programmatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Programmatic
+//
+//  Created by Nix on 11/2/18.
+//  Copyright © 2018 NXN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

b6779a3878be2ce9fcfbb73868121ed0ed93474a
Nix <yiunixon@gmail.com>
2018-11-06 08:02:11 -0800

    write attributed text

--- autolayout_lbta/ViewController.swift
+++ autolayout_lbta/ViewController.swift
@@ -16,13 +16,20 @@ class ViewController: UIViewController {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
         // this enables autolayout for our imageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
+//        textView.text = "Join us today in our fun and games!"
+//        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you read for load and loads of fun? Don't wait any longer! we hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font : UIFont.systemFont(ofSize:  13), NSAttributedStringKey.foregroundColor : UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment = .center
         textView.isEditable = false
@@ -30,26 +37,41 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    private let previousButton: UIButton = {
+       let button = UIButton(type: .system)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // here's our entry point into our app
-        view.addSubview(bearImageView)
+//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
         
         setupLayout()
     }
     
     private func setupLayout() {
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        let topImageContainerView = UIView()
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.addSubview(bearImageView)
+
         
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        NSLayoutConstraint.activate([
+            bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor),
+            bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor),
+            bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5),
+            descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor),
+            descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor),
+            topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+        ])
     }
 
 }

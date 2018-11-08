f0f610a206a062142a9455b35f2dcb8a87424600
Nix <yiunixon@gmail.com>
2018-11-08 12:21:22 -0800

    update from template

--- autolayout_lbta/AppDelegate.swift
+++ autolayout_lbta/AppDelegate.swift
@@ -15,9 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
-        // how to build app in code
+        // this is how we build out our app in code
         window = UIWindow()
         window?.makeKeyAndVisible()
         
@@ -28,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let swipingController = SwipingController(collectionViewLayout: layout)
         
         window?.rootViewController = swipingController
+        
         return true
     }
 
--- autolayout_lbta/SwipingController.swift
+++ autolayout_lbta/SwipingController.swift
@@ -2,8 +2,8 @@
 //  SwipingController.swift
 //  autolayout_lbta
 //
-//  Created by Nix on 11/6/18.
-//  Copyright © 2018 Lets Build That App. All rights reserved.
+//  Created by Brian Voong on 10/12/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
 //
 
 import UIKit
@@ -14,7 +14,7 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .white
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         
         collectionView?.isPagingEnabled = true
     }
@@ -29,11 +29,17 @@ class SwipingController: UICollectionViewController, UICollectionViewDelegateFlo
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .blue
+
+        // definitely don't try this, it is a very bad idea
+//        let imageView = UIImageView()
+//        cell.addSubview(imageView)
+        
+//        cell.backgroundColor = indexPath.item % 2 == 0 ? .red : .green
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.safeAreaLayoutGuide.widthAnchor , height: view.safeAreaLayoutGuide.heightAnchor)
+        return CGSize(width: view.frame.width, height: view.frame.height)
     }
+    
 }
